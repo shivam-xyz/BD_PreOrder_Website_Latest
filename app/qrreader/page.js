2ff48fb9b5a28cd28ad5b2c2e9de1293212b7560
shivam-xyz <shivam9161419412@googlemail.com>
2024-04-07 17:20:57 +0530

    rear camera on

--- app/qrreader/page.js
+++ app/qrreader/page.js
@@ -46,10 +46,12 @@ const Page = () => {
                     <Box sx={{ height: "100%", width: "fit-content", height: { lg: "400px", md: "200px", sm: "200px", xs: "200px" } }}>
                         {cameraAvailable && scanning && (
                             <QrReader
+                            delay={ 300 }
                                 onScan={handleScan}
                                 onError={handleError}
                                 showViewFinder={true}
-                                facingMode={'rear'}
+                                // facingMode={'environment'}
+                                constraints={{aspectRatio: 1,  facingMode: { ideal: "environment" } }}
                                 style={{ width: '100%', height: '100%' }}
                             />
                         )}
