761702faa968fe448cd9319d0a2698c93b5fd2bd
shivam-xyz <shivam9161419412@googlemail.com>
2024-04-07 18:15:55 +0530

    rear camera on

--- app/qrreader/page.js
+++ app/qrreader/page.js
@@ -1,10 +1,10 @@
 'use client'
 import React, { useState, useEffect } from 'react';
 import { Box, Grid, Typography } from '@mui/material';
-import QrReader from 'react-qr-scanner';
 import Header from '@/components/Header';
-import QRCode from 'qrcode.react';
 import Footer from '@/components/Footer';
+import {Html5Qrcode} from 'html5-qrcode';
+// import Html5Qrcode from 'html5-qrcode';
 
 const Page = () => {
     const [scannedData, setScannedData] = useState(null);
@@ -14,6 +14,7 @@ const Page = () => {
 
     useEffect(() => {
         checkCameraPermission();
+        console.log("Library Logs :",Html5Qrcode);
     }, []);
 
     const checkCameraPermission = async () => {
@@ -22,7 +23,7 @@ const Page = () => {
             const videoDevices = devices.filter(device => device.kind === 'videoinput');
             const rearCamera = videoDevices.find(device => device.label.toLowerCase().includes('back') || device.label.toLowerCase().includes('rear'));
             if (rearCamera) {
-              console.log('Rear Camera found : ',rearCamera);
+                console.log('Rear Camera found:', rearCamera);
                 setDeviceId(rearCamera.deviceId);
                 setCameraAvailable(true);
             } else {
@@ -35,16 +36,46 @@ const Page = () => {
         }
     };
 
-    const handleScan = (data) => {
-        if (data) {
-            console.log(data);
-            setScannedData(data);
-            setScanning(false);
+    useEffect(() => {
+        if (cameraAvailable) {
+            startQrCodeScanner();
         }
+        return () => {
+            // Clean up the scanner when component unmounts
+            stopQrCodeScanner();
+        };
+    }, [cameraAvailable]);
+
+    const startQrCodeScanner = () => {
+        const qrCodeScanner = new Html5Qrcode('qr-code-reader');
+        qrCodeScanner.start(
+            deviceId, // Device ID of the rear camera
+            {
+                fps: 10, // Frames per second
+                qrbox: 250 // Size of QR scanning box
+            },
+            (decodedData) => {
+                console.log('Scanned data:', decodedData);
+                setScannedData(decodedData);
+                setScanning(false);
+                stopQrCodeScanner();
+            },
+            (error) => {
+                console.error('QR code scanning error:', error);
+            }
+        );
     };
 
-    const handleError = (error) => {
-        console.error("Error on loading camera", error);
+    const stopQrCodeScanner = () => {
+      Html5Qrcode.getCameras()
+            .then(cameras => {
+                if (cameras && cameras.length > 0) {
+                    cameras.forEach(camera => Html5Qrcode.stop(camera.deviceId));
+                }
+            })
+            .catch(error => {
+                console.error('Error stopping QR code scanner:', error);
+            });
     };
 
     return (
@@ -52,25 +83,19 @@ const Page = () => {
             <Header />
             <Grid container justifyContent={'center'} alignItems={'center'} sx={{}}>
                 <Grid item lg={6} md={4} sm={7} xs={11} sx={{ mt: "60px", height: "60vh", display: "flex", justifyContent: "center", alignItems: "center" }}>
-                    <Box sx={{ height: "100%", width: "fit-content", height: { lg: "400px", md: "200px", sm: "200px", xs: "200px" } }}>
-                        <Typography>Device ID Rear Camera : {deviceId}</Typography>
+                    <Box id="qr-code-reader" sx={{ height: "100%", width: "fit-content", height: { lg: "400px", md: "200px", sm: "200px", xs: "200px" } }}>
                         {cameraAvailable && scanning && (
-                            <QrReader
-                                onScan={handleScan}
-                                onError={handleError}
-                                facingMode="environment"
-                                deviceId={deviceId} // Set the device ID of the rear camera
-                                style={{ width: '400px', height: '400px' }}
-                            />
+                            <Typography variant="subtitle1" align="center">
+                                Scanning QR code...
+                            </Typography>
                         )}
-                        {/* Display scanned QR code image and data if available */}
                         {!scanning && (
                             <Box display="flex" flexDirection="column" alignItems="center">
                                 <Typography variant="subtitle1" align="center">
                                     Scanned Data:
                                 </Typography>
-                                {/* Render the QR code image */}
-                                <QRCode value={scannedData} />
+                                {/* Render the scanned data */}
+                                <Typography>{scannedData}</Typography>
                             </Box>
                         )}
                         {!cameraAvailable && (
